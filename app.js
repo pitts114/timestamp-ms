7a049c56643a6aa0d06b51ba3bf1e87aa55aca92
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-12 19:40:26 -0400

    Parse URL pathname

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //Hello World!
 
-var express = require('express')
+var express = require("express")
+var url = require("url")
+//var chrono = require("chrono-node")
+//chrono.parseDate('An appointment on Sep 12-13')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('*', function(req, res) {
+  res.send('Hello World!')
+  console.log(url.parse(req.url).pathname)
 })
 
 app.listen(app.get('port'), function() {
