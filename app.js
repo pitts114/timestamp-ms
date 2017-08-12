1e2c5be92c3a767b77507ef34a64b1ad90b1fcdd
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-12 18:14:13 -0400

    Hello World with Express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //Hello World!
+var express = require("express")
+var app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.write("Hello World!")
+  res.end()
+})
+
+app.listen(port)
+
+
+
+
+//
