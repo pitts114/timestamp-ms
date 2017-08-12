d9906e288533f9061fd30f1412e8cb4202415a4e
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-12 19:06:34 -0400

    Hello World

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 //Hello World!
-var express = require("express")
-var app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.write("Hello World!")
-  res.end()
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 })
 
-app.listen(port)
-
-
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
 
 //
